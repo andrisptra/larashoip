99e3ea53489560924617dd3ac1f47eb2bebeba99
andrisptra <andrisptra30@gmail.com>
2026-01-18 13:37:46 +0800

    fix my orders

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -75,7 +75,7 @@
                                     d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
                             <p class="mt-2 text-sm text-gray-500">You haven't placed any orders yet</p>
-                            <a href="{{ route('welcome') }}"
+                            <a href="{{ route('products.index') }}"
                                 class="mt-4 inline-block text-sm font-medium text-green-600 hover:text-green-500">Start
                                 Shopping</a>
                         </div>
